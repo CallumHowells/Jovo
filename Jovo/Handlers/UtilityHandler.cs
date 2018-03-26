971fc26dad66293ee92bc6ea450d5c96e29001db
conamatic <connor.thursfield@weightroncb.co.uk>
2018-03-26 16:35:13 +0100

    Actually fixed log history (probably, will find out in 7 days)

--- Jovo/Handlers/UtilityHandler.cs
+++ Jovo/Handlers/UtilityHandler.cs
@@ -16,19 +16,29 @@ namespace Jovo
                 DateTime now = DateTime.Now;
                 DateTime FileCreated = logFile.CreationTime;
 
-                if (now.AddHours(-24) < FileCreated)
+                if (now > FileCreated.AddDays(7))
                 {
-                    LogEvent("Log file was archived", true, true);
+                    LogEvent("Log file is being archived", true, true);
                     if (!Directory.Exists("loghistory"))
                         Directory.CreateDirectory("loghistory");
 
                     logFile.MoveTo("loghistory\\log " + FileCreated.Date.ToString("yyyy-MM-dd") + ".txt");
+                    logFile.Delete();
                 }
-            } catch(Exception) { }
+            } catch (Exception ArchiveEx)
+            {
+                LogEvent(ArchiveEx.ToString());
+            }
         }
 
         public void LogEvent(string message, bool newLine = true, bool blankLine = false)
         {
+            if (!File.Exists("log.txt"))
+            {
+                File.Create("log.txt");
+                File.SetCreationTime("log.txt", DateTime.Now);
+            }
+
             using (StreamWriter LogWriter = new StreamWriter("log.txt", true))
             {
                 if (LogWriter.BaseStream.Position != 0)
