9d0874ac4966fa1095a9fd78198d3a53e83176d8
conamatic <connor.thursfield@weightroncb.co.uk>
2018-07-06 16:12:16 +0100

    Trying to hide mainForm in the distance

--- Jovo/formMain.Designer.cs
+++ Jovo/formMain.Designer.cs
@@ -35,12 +35,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 117);
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(-100000, -100000);
             this.Name = "formMain";
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Background Manager";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formMain_FormClosing);
             this.ResumeLayout(false);
 
